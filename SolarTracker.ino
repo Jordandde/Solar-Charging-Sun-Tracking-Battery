0aefda8c57a03cf75a1e3b89b56b46fd6b06f059
PatrickPLi <lipatrick.1023@gmail.com>
2019-03-08 00:47:02 -0500

    Bad

--- SolarTracker.ino
+++ SolarTracker.ino
@@ -12,19 +12,22 @@ void setup() {
 }
 
 void loop() {
+	
 		int diode1 = analogRead(inPin1);  // Servo 1
 		int diode0 = analogRead(inPin0);  // Servo 0
 
-		Serial.println(diode1);
-		Serial.println(diode0);
+		if (diode1 != 0 || diode0 != 0) {
+			Serial.println(diode1);
+			Serial.println(diode0);
+		}
 
 		if (diode1 > diode0) {
-			diode1 = map(diode1, 0, 1023, 90, 180);
+			diode1 = map(diode1, 0, 1023, 94.5, 180);
 			spinny.write(diode1);
 		}
 
 		else if (diode1 < diode0) {
-			diode0 = map(diode0, 0, 1023, 90, 0);
+			diode0 = map(diode0, 0, 1023, 94.5, 0);
 			spinny.write(diode0);
 		}
 
